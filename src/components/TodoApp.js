a54d11a053dface8749aa6c97aa53338edc08448
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-10-20 14:21:37 +0330

    lifting status state of NavBar component and write filterTodos state for select component

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import NavBar from "./NavBar";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
+  const [filteredTodos, setFilterdTodos] = useState([]);
+  const [status, setStatus] = useState("All");
+  useEffect(() => {
+    filterTodos(status.value);
+  }, [todos, status]);
+
   const addTodo = (todo) => {
     const newTodo = {
       id: Math.floor(Math.random() * 1000),
@@ -24,20 +31,45 @@ const TodoApp = () => {
     const filterTodos = todos.filter((todo) => todo.id !== id);
     setTodos(filterTodos);
   };
-  const updateTodo = (id,newValue) => {
+  const updateTodo = (id, newValue) => {
     const index = todos.findIndex((todo) => todo.id === id);
     const selectedTodo = { ...todos[index] };
-    selectedTodo.text=newValue;
+    selectedTodo.text = newValue;
     const updatedTodo = [...todos];
     updatedTodo[index] = selectedTodo;
     setTodos(updatedTodo);
   };
 
+  const filterTodos = (status) => {
+    switch (status) {
+      case "All":
+        setFilterdTodos(todos);
+        break;
+      case "Completed":
+        setFilterdTodos(todos.filter((todo) => todo.isCompleted));
+        break;
+      case "Uncompleted":
+        setFilterdTodos(todos.filter((todo) => !todo.isCompleted));
+        break;
+      default:
+        setFilterdTodos(todos);
+    }
+  };
+  const changeHandler = (e) => {
+    setStatus(e);
+    filterTodos(e.value);
+  };
+
   return (
     <div className="container">
+      <NavBar
+        unCompletedTodo={todos.filter((todo) => !todo.isCompleted).length}
+        filterdTodos={filterTodos}
+        onChange={changeHandler}
+      />
       <TodoForm submitTodo={addTodo} />
       <TodoList
-        todos={todos}
+        todos={filteredTodos}
         onCompleted={completeTodo}
         onDelete={onDelete}
         onUpdateTodo={updateTodo}
