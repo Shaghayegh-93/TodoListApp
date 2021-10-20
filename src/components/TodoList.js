bb80d78d5af973ce94a364b674950650237c0d36
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-10-20 14:25:27 +0330

    imporove the code

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,13 +2,13 @@ import React from "react";
 import { useState } from "react";
 import Todo from "./Todo";
 import TodoForm from "./TodoForm";
-const TodoList = ({ todos, onCompleted, onDelete,onUpdateTodo }) => {
+const TodoList = ({ todos, onCompleted, onDelete, onUpdateTodo }) => {
   const [edit, setEdit] = useState({ id: null, text: "", isCompleted: false });
- 
- const editTodo=(newValue)=>{
-   onUpdateTodo(edit.id,newValue);
-   setEdit({id:null,text:""})
- }
+
+  const editTodo = (newValue) => {
+    onUpdateTodo(edit.id, newValue);
+    setEdit({ id: null, text: "" });
+  };
   const renderTodos = () => {
     if (todos.length === 0) return <p>add some todo</p>;
     return todos.map((todo) => {
@@ -24,7 +24,11 @@ const TodoList = ({ todos, onCompleted, onDelete,onUpdateTodo }) => {
     });
   };
 
-  return <div>{edit.id ? <TodoForm submitTodo={editTodo} edit={edit} /> : renderTodos()}</div>;
+  return (
+    <div>
+      {edit.id ? <TodoForm submitTodo={editTodo} edit={edit} /> : renderTodos()}
+    </div>
+  );
 };
 
 export default TodoList;
