5a054ae610d14fa7d4d60f377a7cd44d13c23222
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-10-20 14:23:39 +0330

    import react-toastify

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import  "./App.css"
+import "./App.css";
 import TodoApp from "./components/TodoApp";
-import { ToastContainer, Slide } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div className="App">
       <h1>todo list</h1>
-      <TodoApp/>
-      <ToastContainer closeButton={false} position="top-right" transition={Slide}/>
-      
+      <TodoApp />
+      <ToastContainer
+        closeButton={false}
+        position="top-right"
+        transition={Slide}
+      />
     </div>
   );
 };
